56508dc7fc02e9fa88b52ef44ccd82606d6ab045
pledgedavis <pledgedavis14@gmail.com>
2019-11-16 04:15:23 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,10 +11,9 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = "It's-me, Mario"
-  
- 
-  
-  
+
+  expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
 
 
